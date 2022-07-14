cb2c72e52e9dee9e0f0373869d7f112cf7b5fbe7
Anton <deguz@mail.ru>
2022-07-14 22:38:37 +0300

    ssl config was fixed

--- sequelize.js
+++ sequelize.js
@@ -17,7 +17,10 @@ if (process.env.NODE_ENV !== "production") {
     dialect: "postgres",
     protocol: "postgres",
     dialectOptions: {
-      ssl: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   });
 }
