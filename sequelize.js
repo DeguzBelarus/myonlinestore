4d72f539660b32f2cb08f9acc393d1417d524911
Anton <deguz@mail.ru>
2022-07-14 21:09:50 +0300

    database for deployment was configured

--- sequelize.js
+++ sequelize.js
@@ -1,13 +1,23 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+if (process.env.NODE_ENV !== "production") {
+  module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: "postgres",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+    }
+  );
+} else {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    storage: "/database.sql",
-  }
-);
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+}
