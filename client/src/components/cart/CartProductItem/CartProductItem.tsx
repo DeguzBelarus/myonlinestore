5e863b79b01d1d08a7f83b0ed045ea4846e74ade
Anton <deguz@mail.ru>
2022-08-02 18:01:38 +0300

    cart product item and cart components styles were optimised

--- client/src/components/cart/CartProductItem/CartProductItem.tsx
+++ client/src/components/cart/CartProductItem/CartProductItem.tsx
@@ -2,35 +2,58 @@ import { FC } from "react";
 import { useAppSelector, useAppDispatch } from "../../../app/hooks";
 
 import {
-   deleteCartProductAsync
+   deleteCartProductAsync,
+   addCartProductAsync
 } from "../../../app/userSlice";
 import { getCurrentLanguage } from "../../../app/globalSlice";
-import { CartProduct, getToken } from "../../../app/userSlice";
+import { getToken, getUserId } from "../../../app/userSlice";
+import { ProductInCartModified } from "../../../pages/ShoppingCartPage/ShoppingCartPage";
 import "./CartProductItem.scss"
 
 interface Props {
-   data: CartProduct
+   data: ProductInCartModified
 }
 
 export const CartProductItem: FC<Props> = ({ data }) => {
    const dispatch = useAppDispatch()
 
    const token: string = useAppSelector(getToken)
+   const userId: string = useAppSelector(getUserId)
    const currentLanguage: string = useAppSelector(getCurrentLanguage)
 
    const removeCartProductHandler = (id: string) => {
       dispatch(deleteCartProductAsync({ id: id, token: token, lang: currentLanguage }))
    }
 
+   const addProductToCart = () => {
+      dispatch(addCartProductAsync({
+         lang: currentLanguage,
+         token: token,
+         data: { id: userId, productId: data.productId }
+      }))
+   }
    return <div className="cart-product-item-wrapper">
-      <span>{data.details?.name}</span>
-      <span>{`${data.details?.price} USD`}</span>
+      <img
+         className="poster"
+         src={`/${data.productTypeId}/${data.productBrandId}/${data.productName}/${data.productPoster}`}
+         draggable={false}
+         alt="a product preview" />
+      <span>{data.productName}</span>
+      <span>{`${data.productPrice} USD`}</span>
       <button
+         type="button"
          className="remove-cart-product-button"
-         onClick={() => removeCartProductHandler(data.id.toString())}>
-         {currentLanguage === "ru"
-            ? "Удалить"
-            : "Remove"}
-      </button>
+         onClick={() => removeCartProductHandler(data.cartItemId.toString())}>
+         -</button>
+      <span>{currentLanguage === "ru"
+         ? `${data.quantity} шт.`
+         : `${data.quantity} pcs.`}
+      </span>
+      <button
+         type="button"
+         className="add-cart-product-button"
+         onClick={addProductToCart}>
+         +</button>
+      <span>{`${data.sum} USD`}</span>
    </div>
 }
--- client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
+++ client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 
 import { setCartPageIsActive } from "../../app/shopSlice";
@@ -7,16 +7,75 @@ import { getProductsInCart, CartProduct } from "../../app/userSlice";
 import "./ShoppingCartPage.scss"
 import { CartProductItem } from "../../components/cart/CartProductItem/CartProductItem";
 
+export interface ProductInCartModified {
+   cartItemId: number,
+   cartProductid: number,
+   cartId: number,
+   productId: number,
+   productName?: string,
+   productPrice?: number,
+   productRating?: number,
+   productPoster?: string,
+   productTypeId?: number,
+   productBrandId?: number,
+   quantity: number,
+   sum: number
+}
+
 export const ShoppingCartPage: FC = () => {
    const dispatch = useAppDispatch()
 
    const currentLanguage = useAppSelector(getCurrentLanguage)
    const productsInCart: CartProduct[] = useAppSelector(getProductsInCart)
+   const [productsInCartModified, setProductsInCartModified] = useState<ProductInCartModified[]>([])
+
+   const sortCartProductsMethod = (previous: ProductInCartModified, next: ProductInCartModified) => {
+      switch (true) {
+         case previous.cartProductid > next.cartProductid:
+            return 1
+         case previous.cartProductid < next.cartProductid:
+            return -1
+         default:
+            return 0
+      }
+   }
 
    useEffect(() => {
       document.title = currentLanguage === "ru" ? "MyOnlineStore: Ваша Корзина" : "MyOnlineStore: Your Cart"
       document.documentElement.lang = currentLanguage === "ru" ? "ru" : "en"
    }, [currentLanguage])
+
+   useEffect(() => {
+      setProductsInCartModified(
+         productsInCart
+            .map((cartItem: CartProduct, index, array) => {
+               const productId = cartItem.productId
+
+               return {
+                  cartItemId: cartItem.id,
+                  cartProductid: productId,
+                  cartId: cartItem.cartId,
+                  productId: cartItem.productId,
+                  productName: cartItem.details?.name,
+                  productPrice: cartItem.details?.price,
+                  productRating: cartItem.details?.rating,
+                  productPoster: cartItem.details?.poster,
+                  productTypeId: cartItem.details?.productTypeId,
+                  productBrandId: cartItem.details?.productBrandId,
+                  quantity: array.filter((cartItem: CartProduct) => cartItem.productId === productId).length,
+                  sum: array.reduce((sum: number, cartItem: CartProduct) => {
+                     if (cartItem.productId === productId) {
+                        return sum += cartItem.details?.price || sum
+                     } else return sum
+                  }, 0)
+               }
+            })
+            .sort(sortCartProductsMethod)
+            .reduce((unique: ProductInCartModified[], cartItemModified: ProductInCartModified) => {
+               return unique.find((uniqueElement: ProductInCartModified) => uniqueElement.cartProductid === cartItemModified.cartProductid) ? unique : [...unique, cartItemModified]
+            }, [])
+      )
+   }, [productsInCart])
 
    useEffect(() => {
       dispatch(setCartPageIsActive(true))
@@ -26,10 +85,10 @@ export const ShoppingCartPage: FC = () => {
       }
    }, [])
    return <div className="shopping-cart-page-wrapper">
-      <div className={productsInCart.length ? "cart-items-wrapper" : "cart-items-wrapper empty"}>
-         {productsInCart.length
-            ? productsInCart.map((cartItem: CartProduct) => {
-               return <CartProductItem data={cartItem} key={cartItem.id} />
+      <div className={productsInCartModified.length ? "cart-items-wrapper" : "cart-items-wrapper empty"}>
+         {productsInCartModified.length
+            ? productsInCartModified.map((cartItemModified: ProductInCartModified) => {
+               return <CartProductItem data={cartItemModified} key={cartItemModified.cartProductid} />
             })
             : <span
                className="about-emptiness-span">
@@ -38,5 +97,14 @@ export const ShoppingCartPage: FC = () => {
                   : "Your Shopping cart is empty :("}
             </span>}
       </div>
+      <p className="total-cost-paragraph">
+         {currentLanguage === "ru" ? "Итого: " : "Total: "}
+         <span>
+            {`${productsInCart.reduce((sum: number, cartItem: CartProduct) => {
+               return sum += cartItem.details?.price || sum
+            }, 0)} `}
+            USD
+         </span>
+      </p>
    </div>
 }
